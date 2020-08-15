25aeabf6160287386c37149c7d0c3804ad3649ee
sllllllle <lac85at@gmail.com>
2020-08-15 14:32:11 +0900

    応援/応援解除を非同期で行うように修正

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,16 +1,26 @@
 class SupportsController < ApplicationController
   before_action :require_user_logged_in
 
+  # def create
+  #   record = Record.find(params[:support_id])
+  #   current_user.support(record)
+  #   redirect_back(fallback_location: '/')
+  # end
+
   def create
-    record = Record.find(params[:support_id])
-    current_user.support(record)
-    redirect_back(fallback_location: '/')
+    @record = Record.find(params[:support_id])
+    @yell = current_user.support(@record)
   end
 
+  # def destroy
+  #   record = Record.find(params[:support_id])
+  #   current_user.unsupport(record)
+  #   redirect_back(fallback_location: '/')
+  # end
+
   def destroy
-    record = Record.find(params[:support_id])
-    current_user.unsupport(record)
-    redirect_back(fallback_location: '/')
+    @record = Record.find(params[:support_id])
+    @yell = current_user.unsupport(@record)
   end
 
   def supporters
